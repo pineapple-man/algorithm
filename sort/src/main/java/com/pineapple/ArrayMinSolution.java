ccce83a9fe4e56353bf7072614d9b97c702351ac
mingchaohuHost <1437649071@qq.com>
2022-05-07 13:42:35 +0800

    docs(sort): add program
    区间上的荷兰国旗问题，只是增加了函数原型，需要完成对应的功能

---
+++ sort/src/main/java/com/pineapple/ArrayMinSolution.java
@@ -0,0 +1,22 @@
+package com.pineapple;
+
+/**
+ * @author pineapple-man
+ * @date 2022-05-07 13:18
+ */
+public class ArrayMinSolution {
+	/**
+	 * 使用 bfprt 查找数组 arr 的 第 k 小数
+	 */
+	public static int bfprt(int[] arr, int k) {
+		//todo implement bfprt
+		return 0;
+	}
+	
+	/**
+	 * 查找 arr 的第 K 小的数
+	 */
+	public static int arrKthMin(int[] arr, int k) {
+		return 0;
+	}
+}
---
+++ sort/src/test/java/com/pineapple/ArrayMinSolutionTest.java
@@ -0,0 +1,18 @@
+package com.pineapple;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pineapple-man
+ * @date 2022-05-07 13:26
+ */
+class ArrayMinSolutionTest {
+	
+	@Test
+	void bfprt() {
+	}
+	
+	@Test
+	void arrKthMin() {
+	}
+}

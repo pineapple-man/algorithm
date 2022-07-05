3c466921419b9f69487d0fe45cd652939b9d8c84
mingchaohuHost <1437649071@qq.com>
2022-07-05 17:48:12 +0800

    feat(*): remove file

--- dynamic-programing/attempt-model/src/main/golang/minStickers.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func minStickers(stickers []string, target string) int {
-       stickBucket := make([][]byte,0,len(stickers))
-       for _,sticker:=range stickers{
-              stickBucket = append()
-       }
-}
